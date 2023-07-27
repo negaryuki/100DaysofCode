4912f4d812fd1bce3ec005257bfe495674256d74
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-27 23:35:50 +0430

    Day 02 exercise

--- Day01/Exercise01.py
+++ Day01/Exercise01.py
@@ -1,5 +1,7 @@
 print("Day 1 - Python Print Function")
 print("the function is declared like this:")
-print("print"'(what to print)')
 
+print("print"'(what to print)')
 print("print(what to print)")
+
+print("print"'("what to print")')
