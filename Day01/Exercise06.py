32dd0e76fe0d67afb3c816999fe3e33853cdda83
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-28 01:14:07 +0430

    Day 02 exercise

--- Day01/Exercise06.py
+++ Day01/Exercise06.py
@@ -1,7 +1,7 @@
 print("Hello and welcome to the AI Band Name Generator!")
-city=(input("What's the name of the city you grew up in?"))
-pet=(input("What's the name of your first pet?"))
+city=(input("What's the name of the city you grew up in?\n" ))
+pet=(input("What's the name of your first pet?\n"))
 
-brand_name = city + pet
+band_name = city + "" + pet
 
-print("your Band Name could be " + brand_name)
+print("your Band Name could be " + band_name)
