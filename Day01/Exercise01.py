f115f8b79db6dc440305035fa14bd70aa2d9b38f
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-27 23:32:38 +0430

    Day 02 exercise

--- Day01/Exercise01.py
+++ Day01/Exercise01.py
@@ -1,4 +1,5 @@
 print("Day 1 - Python Print Function")
-print("the fuction is decleared like this:")
-print("print"'what to print')
+print("the function is declared like this:")
+print("print"'(what to print)')
 
+print("print(what to print)")
--- Day01/Exercise03.py
+++ Day01/Exercise03.py
@@ -1,4 +1,4 @@
 print("Day 1 - String Manipulation")
-print("string concatenation is done with the '+' sign.")
+print('string concatenation is done with the "+" sign.')
 print('e.g. print("Hello" + "world")')
 print("New lines can be created with a backslash and n.")
