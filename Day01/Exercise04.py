482413a4c6e050deb15e7a04ca09a9ef86e1d500
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-27 23:50:16 +0430

    Day 02 exercise

---
+++ Day01/Exercise04.py
@@ -0,0 +1,10 @@
+# Input Function : gets beck information from the user
+
+input("What is your Name?")
+# This is a Comment -> in order to write a comment just add "#" to the beginning of the sentence
+
+print("Hello" + " " + input("What is your Name?"))
+
+# Exercise 03:
+
+print(len(input("what is your name")))
