6cb0b8bdff963631e1b43edb5611b99bf6d911eb
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-28 13:21:25 +0430

    Day 02 Lesson

---
+++ Day02/Main_Lesson.py
@@ -0,0 +1,14 @@
+#Day No.2
+
+#Data Types:
+# 1. Strings -> "Hello" / Characters => print("World"[0])  =>> W / anything inside "" is considered as a string
+print("Hello" [4])
+
+# 2. Integer -> Numbers without decimals / 1234 /
+# for dividing numbers with a comma use _ e.g. 123_456_000 is : 123,4456,000
+print(123 + 456)
+print(123_345_567)
+
+# 3. Floats -> Numbers with decimals / 123.34
+
+# 4. Boolean -> True / False
