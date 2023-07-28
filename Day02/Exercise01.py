cf3e05377ccbbd5cbbe60fbdf50164542ffbe38a
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-28 13:50:27 +0430

    Day 02 Lesson

--- Day02/Exercise01.py
+++ Day02/Exercise01.py
@@ -2,6 +2,10 @@
 
 num_char = len(input("what is your name?\n"))
 
+print(type(num_char))
+
 new_num_char = str(num_char)
+
+print(type(new_num_char))
 
 print("your name has " + new_num_char + " characters")
---
+++ Day02/Exercise02.py
@@ -0,0 +1,12 @@
+#write a program that adds the digits in a 2 digit number
+# e.g. if the input was 35, then the inputs e.g. should be 3 + 5 = 8.
+
+entered_number = input("write a 2 digit Number\n")
+
+first_digit = int(entered_number[0])
+second_digit = int(entered_number[1])
+
+
+result = str(first_digit+ second_digit)
+
+output = print(" The result is: " + result)
