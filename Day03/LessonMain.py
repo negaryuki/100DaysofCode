01c6ede029c041973f98678a49f9961284f28058
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-29 15:13:09 +0430

    Day 02 Lesson

---
+++ Day03/LessonMain.py
@@ -0,0 +1,10 @@
+# Conditional Statements
+# if CONDITION: DO THIS else: DO THIS
+
+height = int(input("What is your height in cm?"))
+
+if height == 120:
+    print("You can ride the roller coaster")
+else:
+    print("You Can't ride the roller coaster")
+
