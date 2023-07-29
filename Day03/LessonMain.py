85c39ad88fca142a5008d2da7b9632ee1388d321
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-29 15:43:51 +0430

    Day 03

--- Day03/LessonMain.py
+++ Day03/LessonMain.py
@@ -10,3 +10,31 @@ else:
 
 # Modulo operator !! : 7 % 2 = 2 + 2 + 2 + (1) ==> 7 % 2 = 1
 
+print(9 % 2)
+
+# Nested if/else statement:
+
+age = int(input("What is Your age?"))
+
+if height >= 120:
+    print("You can ride the roller coaster")
+    if age <= 18:
+        print("please pay 7$")
+    else:
+        print("please pay 12$")
+else:
+    print("You Can't ride the roller coaster")
+
+
+# if/elif/ else
+
+if height >= 120:
+    print("You can ride the roller coaster")
+    if age <= 12:
+        print("Please pay 5$")
+    elif 12 > age <= 18:
+        print("Please Pay 7 $")
+    else:
+        print("please Pay 12$")
+else:
+    print("you can not ride the roller coaster")
