4ce9a57be368370465c518dc607bf9374a5d7863
Negar Norouzi <negar.norouzi94@gmail.com>
2023-07-30 20:12:45 +0430

    Day 04

---
+++ Day04/Exercise01 - Head and Tail.py
@@ -0,0 +1,14 @@
+# create a virtual coin toss program. it will randomly tell the user "Heads or "Tails"
+
+import random
+
+
+
+random = random.randint(0, 1)
+
+print(random)
+
+if random == 1:
+    print("Heads")
+else:
+    print("Tails")
---
+++ Day04/MainLesson.py
@@ -0,0 +1,14 @@
+# randomisation MODULE
+
+import random
+import MyModule
+
+random_integer = random.randint(1, 25)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+# a random decimal no. between 0 and 5
+random_decimal = random.random() * 5
+print(random_decimal)
