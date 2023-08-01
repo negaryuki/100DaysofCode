06405f4ff7c0f3ee83c445f6c57014841b650c4a
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-01 20:18:03 +0430

    Day 05   *v*

---
+++ Day06/Mainlesson.py
@@ -0,0 +1,21 @@
+# Functions
+
+# creating a function
+
+def my_function():
+    print("Hello")
+    print("bye")
+
+my_function()
+
+sky = input("How's the weather?")
+
+def new_function():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("hello")
+print("world")
+
+new_function()
