c1ee1f5fcd771d4f0f5ee1a61b9dcd9427053f84
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-04 16:35:49 +0430

    Hangman day 7

---
+++ Day08/main.py
@@ -0,0 +1,19 @@
+# Symple Function
+
+def greet():
+    print("Hi")
+    print("How do you do?")
+    print("Isn't the Weather lovely today?")
+
+greet()
+
+# Function that allows for input (Parameter / Argument)  (Parameter) name = (Argument) Emilia
+
+def greet_with_name(name):
+    print(f'Hi {name}')
+    print(f"How do you do {name}?")
+    print(f"Isn't the Weather lovely today {name}?")
+
+greet_with_name("Emilia")
+
+greet_with_name("Goerge")
