8e0d156d2599ae652b7c49b639126c0c59da12ba
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-04 16:58:02 +0430

    Day 8 - Exercise01-Area Calculation
    You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
    
    number of cans = (wall height ✖️ wall width) ÷ coverage per can.
    
    e.g. Height = 2, Width = 4, Coverage = 5
    
    number of cans = (2 ✖️ 4) ÷ 5

---
+++ Day08/Exercise01-Area Calculation.py
@@ -0,0 +1,16 @@
+#Write your code below this line 👇
+
+# number of cans = (wall height ✖️ wall width) ÷ coverage per can.
+
+def paint_calc(height, width, cover):
+    print(round((height * width) / cover))
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
