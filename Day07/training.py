129dabf533af5dd42c948789a31ef04a8dc8aa12
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-04 13:27:14 +0430

    Hangman day 7

--- Day07/training.py
+++ Day07/training.py
@@ -4,6 +4,15 @@ word_list = ["ardvark", "baboom", "camel"]
 
 chosen_word = random.choice(word_list)
 
+print(f'The Original word is {chosen_word}')
+
+display = []
+
+for letter in chosen_word:
+    display += "_"
+
+print(display)
+
 guess = input("Guess a letter").lower()
 
 for letter in chosen_word:
