d8192f89011ba18b3d3cf3159e85aa312cbac895
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-06 20:32:32 +0430

    wrote ceasar() function

---
+++ Day08/Final Project - Ceasar cipher (improved Ver.).py
@@ -0,0 +1,36 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("type 'encode' to encrypt, type 'decode' to decrypts:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("type the shift number\n"))
+
+
+# don't change the above:
+
+# To DO -1: combine the encrypt and decrypt function into a single function called ceasar().
+
+
+def ceasar(ceasar_text, ceasar_shift, ceasar_direction):
+    end_text = ""
+
+    if ceasar_direction == "encode":
+        for letter in ceasar_text:
+            position = alphabet.index(letter)
+            new_position = position + ceasar_shift
+            new_letter = alphabet[new_position]
+            end_text += new_letter
+        print(f'Your encoded text is: {end_text}')
+
+    elif ceasar_direction == "decode":
+        for letter in ceasar_text:
+            position = alphabet.index(letter)
+            new_position = position - ceasar_shift
+            new_letter = alphabet[new_position]
+            end_text += new_letter
+        print(f'Your decoded text is: {end_text}')
+
+
+
+ceasar(text,shift,direction)
