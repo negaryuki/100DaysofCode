2afe3b52c3a8534f5250b8f89a8bc7a7abe21ecf
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-06 21:48:57 +0430

    wrote ceasar() function

--- Day08/Final Project - Ceasar cipher (improved Ver.).py
+++ Day08/Final Project - Ceasar cipher (improved Ver.).py
@@ -7,12 +7,9 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
             'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("type 'encode' to encrypt, type 'decode' to decrypts:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("type the shift number\n"))
 
 
-# don't change the above:
+
 
 # To DO -1: combine the encrypt and decrypt function into a single function called ceasar().
 
@@ -44,10 +41,25 @@ def ceasar(ceasar_text, ceasar_shift, ceasar_direction):
         ceasar_shift *= -1
 
     for letter in ceasar_text:
-        position = alphabet.index(letter)
-        new_position = position + ceasar_shift
-        end_text += alphabet[new_position]
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + ceasar_shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += letter
     print(f'Your {ceasar_direction} text is: {end_text}')
 
 
-ceasar(text, shift, direction)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 25
+
+    ceasar(text, shift, direction)
+
+    result = input('Would you like to repeat the program again? "Yes" or "No"\n').lower()
+    if result == "no":
+        should_continue = False
+        print("Program ended...")
