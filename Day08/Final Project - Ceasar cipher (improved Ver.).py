95df545ca3e32a70c19199f55636049b9af0b691
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-06 21:02:35 +0430

    wrote ceasar() function

--- Day08/Final Project - Ceasar cipher (improved Ver.).py
+++ Day08/Final Project - Ceasar cipher (improved Ver.).py
@@ -12,25 +12,37 @@ shift = int(input("type the shift number\n"))
 # To DO -1: combine the encrypt and decrypt function into a single function called ceasar().
 
 
+# def ceasar(ceasar_text, ceasar_shift, ceasar_direction):
+#     end_text = ""
+#
+#     if ceasar_direction == "encode":
+#         for letter in ceasar_text:
+#             position = alphabet.index(letter)
+#             new_position = position + ceasar_shift
+#             new_letter = alphabet[new_position]
+#             end_text += new_letter
+#         print(f'Your encoded text is: {end_text}')
+#
+#     elif ceasar_direction == "decode":
+#         for letter in ceasar_text:
+#             position = alphabet.index(letter)
+#             new_position = position - ceasar_shift
+#             new_letter = alphabet[new_position]
+#             end_text += new_letter
+#         print(f'Your decoded text is: {end_text}')
+
+# or we could just simplify it as below:
+
 def ceasar(ceasar_text, ceasar_shift, ceasar_direction):
     end_text = ""
+    if ceasar_direction == "decode":
+        ceasar_shift *= -1
 
-    if ceasar_direction == "encode":
-        for letter in ceasar_text:
-            position = alphabet.index(letter)
-            new_position = position + ceasar_shift
-            new_letter = alphabet[new_position]
-            end_text += new_letter
-        print(f'Your encoded text is: {end_text}')
-
-    elif ceasar_direction == "decode":
-        for letter in ceasar_text:
-            position = alphabet.index(letter)
-            new_position = position - ceasar_shift
-            new_letter = alphabet[new_position]
-            end_text += new_letter
-        print(f'Your decoded text is: {end_text}')
+    for letter in ceasar_text:
+        position = alphabet.index(letter)
+        new_position = position + ceasar_shift
+        end_text += alphabet[new_position]
+    print(f'Your {ceasar_direction} text is: {end_text}')
 
 
-
-ceasar(text,shift,direction)
+ceasar(text, shift, direction)
