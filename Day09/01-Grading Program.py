66c73d18572d61a816308844726502212c9e5b0b
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-07 19:54:44 +0430

    explanation about DDictionaries

---
+++ Day09/01-Grading Program.py
@@ -0,0 +1,10 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99,
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
--- Day09/Main.py
+++ Day09/Main.py
@@ -8,3 +8,32 @@ programming_dictionary = {"Bug": " an error in a program that ....",
 # in lists we would use index [0]
 
 print(programming_dictionary["Bug"])
+
+# add new entry to dictionary: Dictionary["key"] = value
+
+programming_dictionary["Loop"] = " a piece of code that ..."
+print(programming_dictionary)
+
+# Creating an empty Dictionary:
+
+empty_dictionary = {}
+print(empty_dictionary)
+
+# wiping a dictionary:
+
+# programming_dictionary={}
+# print(programming_dictionary)
+
+# edit an item in a dictionary:
+
+programming_dictionary["Bug"] = " this text was replaced"
+
+print(programming_dictionary)
+
+# Loop through a dictionary:
+
+for thing in programming_dictionary:
+    print(thing)
+    print(programming_dictionary[thing])
+
+
