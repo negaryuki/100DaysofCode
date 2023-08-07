e82daab98f8ffec0479aec8ee0650048ebd8709a
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-07 19:40:54 +0430

    explanation about DDictionaries

---
+++ Day09/Main.py
@@ -0,0 +1,10 @@
+# Dictionary and nesting
+
+# Creating a Dictionary {key: value}
+programming_dictionary = {"Bug": " an error in a program that ....",
+                          "Function": "is ..."}
+
+# retrieve an item from dictionary :
+# in lists we would use index [0]
+
+print(programming_dictionary["Bug"])
