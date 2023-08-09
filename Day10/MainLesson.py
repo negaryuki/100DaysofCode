605ad789ce2f0fe54fd6fdcf620cdfe6ee022468
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-08-09 15:25:47 +0330

    Update MainLesson.py

--- Day10/MainLesson.py
+++ Day10/MainLesson.py
@@ -1,6 +1,11 @@
 # Function with Outputs
 
+
+# This function returns an early output by using "return"
+
 def format_name (name,lastname):
+    """ takes a name and a last name and changes it to title mode"""
+
     if name == "" or lastname == "":
         return "You did not provide valid inputs"
     
@@ -11,3 +16,10 @@ def format_name (name,lastname):
     
 
 print(format_name (input("what is your name"), input("what is your last name")))
+
+"""
+Docstrings: are strings that explain more about the fuction we wrote  ---> """ The text is written here"""
+Using docstrings allows us to write as many lines as we want
+Docstrings can also be used as multiline comments
+Python suggests not using this for commenting.
+"""
