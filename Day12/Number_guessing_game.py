419e851afe11abf9fd703ab664aa31b2d8a2a882
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-13 19:58:17 +0430

    finally done

--- Day12/Number_guessing_game.py
+++ Day12/Number_guessing_game.py
@@ -1,53 +1,68 @@
-#creating. number guessing game
+# creating. number guessing game
 
 import random
 
+from art import logo
+
+
+
+
 def randomize_number():
-  number = random.randint(1,100)
-  return number 
+    number = random.randint(1, 100)
+    return number
+
+
+hard_attempt = 5
+easy_attempt = 10
+
+
+def set_difficulty():
+    game_continue = True
+    level = input(
+        'Welcome to the number guessing game ^o^/ ! your goal is to guess what random number the computer chose '
+        'between 1 and 100.\n first choose your level. \'hard\' or \'easy\'?').lower()
+
+    while game_continue:
+
+        if level == "hard":
+            return hard_attempt
+
+        elif level == "easy":
+            return easy_attempt
+
+        else:
+            print("you entered an invalid input. the game will now restart")
+            game_continue = False
+            set_difficulty()
 
 
 def game_fundamental():
     number = randomize_number()
-    continue_guess = True 
-    
-    while continue_guess == True:
+    continue_guess = True
+    attempt = set_difficulty()
+
+    while continue_guess:
         guess = int(input("Enter a number\n"))
+
         if guess > number:
-            print(" You're too high")
             attempt -= 1
-        elif guess < number:
-            print("You're too low")
+            print(f'too high you have {attempt} attempts left')
+
+        if guess < number:
             attempt -= 1
-        elif guess == number:
+            print(f'too low you have {attempt} attempts left')
+
+        if guess == number:
             print("You guessed right! You win!")
-        elif attempt == 0:
+
+        if attempt == 0:
             continue_guess = False
             print("you are out of tries. Game over")
-    
-            
 
-def game_start():
-  game_continue = True
-  level = print(input("Welcome to the number guessing game ^o^/ ! your goal is to guess what random number the computer chose between 1 and 100.\n first choose your level. 'hard' or 'easy'?"))
 
-  
-  while game_continue == True:
-        
-      if level == "hard":
-        attempt = 5
-        game_fundamental()
+if input("Do you wanna guess the randomized number? 'y' or 'n'").lower() =='y':
+    print(logo)
+    game_fundamental()
+else:
+    print("ByeBye See you next time! ")
 
-      elif level == "easy":
-        attempt = 10
-        game_fundamental()
-        
-      else:
-        print("you entered an invalid input. the game will now restart")
-        game_continue = False
-        game_start()
-
-      
-    
-  
-game_start()    
---
+++ Day12/art.py
@@ -0,0 +1,25 @@
+logo = """
+
+                                           
+                                                                              
+                                           88                                 
+                                           88                                 
+                                           88                                 
+8b,dPPYba,  88       88 88,dPYba,,adPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+88P'   `"8a 88       88 88P'   "88"    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+88       88 88       88 88      88      88 88       d8 8PP""""""" 88          
+88       88 "8a,   ,a88 88      88      88 88b,   ,a8" "8b,   ,aa 88          
+88       88  `"YbbdP'Y8 88      88      88 8Y"Ybbd8"'   `"Ybbd8"' 88          
+                                                                              
+                                                                              
+           
+           
+           
+           
+,adPPYba,  
+I8[    ""  
+ `"Y8ba,   
+aa    ]8I  
+`"YbbdP"'  
+           
+"""
