01e045489cea507f510429387ac0686be47860eb
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-08-14 11:29:33 +0330

    Update Debuggin_lessons.py

--- Day13/Debuggin_lessons.py
+++ Day13/Debuggin_lessons.py
@@ -60,3 +60,21 @@ elif year >= 1994:
     print(" you are Gen Z")
     
 #-------------------------------------------------------------------------------------------
+
+# Print is your Friend. use it to debug the codes
+
+#pages = 0
+#word_per_page = 0
+#pages = int (input("number of pages: "))
+#word_per_page == int(input("number of words per pages"))
+#total_words = pages * word_per_page
+#print(total_words)
+
+
+
+pages = 0
+word_per_page = 0
+pages = int(input("number of pages: "))
+word_per_page = int(input("number of words per pages"))
+total_words = pages * word_per_page
+print(total_words)
