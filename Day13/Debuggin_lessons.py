02ffbcef9a26fb32f425842f6c0b9ea1b111fbc2
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-08-14 11:09:41 +0330

    Create Debuggin_lessons.py

---
+++ Day13/Debuggin_lessons.py
@@ -0,0 +1,40 @@
+# Debug the below codes:
+
+#def my_function():
+#    for i in range(1,20):
+#        if i ==20:
+#            print("you got it")
+#            
+            
+#my_function()
+
+
+# Explaining the problem: since "i" will loop through 1 to 19 => i = 20 will never occur
+
+def my_function():
+    for i in range(1,21):
+        if i ==20:
+            print("you got it")
+            
+            
+my_function()
+
+#-------------------------------------------------------------------------------------------
+# Reproducing buggs 
+
+
+
+#from random import randint 
+#dice_imgs =["1", "2" , "3" , "4", "5" , "6"]
+#dice_num = randint(1,6)
+#print(dice_imgs[dice_num])
+
+
+# The bug is that occasionally there might be an error and it's because list index starts from 0 => in this example the index range is from 0,5 
+
+from random import randint 
+dice_imgs =["1", "2" , "3" , "4", "5" , "6"]
+dice_num = randint(0,5)
+print(dice_imgs[dice_num])
+
+#-------------------------------------------------------------------------------------------
