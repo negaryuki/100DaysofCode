bdbd32f8c1ee57e60fd0c1deb45d497807301bd9
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-08-15 16:37:37 +0330

    Update 14 - Lower_higher_game.py

--- Day14/14 - Lower_higher_game.py
+++ Day14/14 - Lower_higher_game.py
@@ -8,7 +8,7 @@ from game_data import data
 def random_character():
     character = random.choice(data)
 
-    return(character)
+    return(character)        
         
     
 def compare_followers():
@@ -19,11 +19,24 @@ def compare_followers():
     a_followers = a_character['follower_count']
     b_followers = b_character['follower_count']
     
+    print(f" Character A : {a_character['name']} , {a_character['description']} from {a_character['country']}")
+
+    print(f" Character B : {b_character['name']} , {b_character['description']} from {b_character['country']}")
+
+    guess = input(" Who has more Followers? 'a' or 'b' ")
+    
+    
+    if a_followers > b_followers and guess == "a":
+        return('You are right. Next:')
+     
+    elif a_followers > b_followers and guess == "b":
+         return("wrong. You loose")
+
+    elif a_followers < b_followers and guess == "b":
+        return('You are right. Next:')
+     
+    elif a_followers < b_followers and guess == "a":
+         return("wrong. You loose")
 
 
-    return(a_followers , b_followers)
-    
-        
-    
-print(random_character())
 print(compare_followers())
