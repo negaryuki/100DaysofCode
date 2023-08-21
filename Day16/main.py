cd57dbe4a99dd1432bceef7d6f08e4f5d690abf0
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-08-21 10:41:38 +0330

    Create main.py

---
+++ Day16/main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle ,  Screen
+
+timmy = Turtle()
+
+print(timmy)
+
+# object.attribute    from object get attribute
+timmy.shape("turrtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvas_view)
+
+
+# object.method().  from object call Method (function)
+
+my_screen.exitonclick()
+
+
+
+
