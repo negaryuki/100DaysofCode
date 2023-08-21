f367ab9f4694c2fbe952740d154956113080712f
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-08-21 10:44:50 +0330

    Update Pretty_Table_practice.py

--- Day16/Pretty_Table_practice.py
+++ Day16/Pretty_Table_practice.py
@@ -5,3 +5,12 @@ table = PrettyTable()
 table.add_column("Pokemon Name" , ["Pikachu" , "Squirtle" , "Charmander"])
 
 table.add_column("Type" , ["Electric" , "Water" , "Fire"])
+
+
+#Change object attribute:
+
+table.align = "l"
+
+print(table)
+
+
