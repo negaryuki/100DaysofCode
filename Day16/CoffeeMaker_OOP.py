0f286a093d232dfe29705b9307549ddfcb34fee7
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-08-21 15:03:36 +0330

    Update CoffeeMaker_OOP.py

--- Day16/CoffeeMaker_OOP.py
+++ Day16/CoffeeMaker_OOP.py
@@ -1,3 +1,36 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+#Step 1: Print Report
+#Step 2: Check resoources sufficient?
+#Step 3: Process Coin
+#Step 4: Check transaction succesful
+#Step 5: Make Coffee
+
+money_machine = MoneyMachine()
+coffee_maker =CoffeeMaker()
+menu = Menu()
+is_on = True
+
+while	is_on:
+	options = menu.get_items()
+	choice = input(f'what would you like? {options}').lower()
+	
+	if choice == "off":
+		is_on = False
+	elif choice == "report":
+		money_machine.report()
+		coffee_maker.report()
+		
+	else:
+		drink = menu.find_drink(choice)
+		if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+				coffee_maker.make_coffee(drink)		 
+		 
+		
