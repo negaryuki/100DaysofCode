32468915b6fd9ba7737e20062075863301a60757
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-26 09:25:50 +0430

    working on the code of the gam

--- Day17/main.py
+++ Day17/main.py
@@ -1,6 +1,6 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -9,3 +9,10 @@ for question in question_data:
     new_answer = question["answer"]
     new_question = Question(new_text,new_answer)
     question_bank.append(new_question)
+
+# To Do 1 asking Q
+# To Do 2 checking if answer was correct
+# check if we are at the end of the quiz
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
