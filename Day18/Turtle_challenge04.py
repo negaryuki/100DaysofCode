96087b91026bc1ea2ef157850de7573d2638ca86
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-29 10:54:14 +0430

    generate random walk

--- Day18/Turtle_challenge04.py
+++ Day18/Turtle_challenge04.py
@@ -1,19 +1,27 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
 walk = Turtle()
+turtle.colormode(255)
 
-color = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple",
-         "navy", "blue", "skyblue","turquoise", "lightgreen",
-          "green", "darkgreen", "chocolate", "brown", "black", "gray"
-]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb = (r, g, b)
+    return rgb
+
 
 directions = [0, 90, 180, 270]
+walk.pensize(18)
+walk.speed("fast")
 
 for loop in range(200):
     walk.forward(30)
     walk.setheading(random.choice(directions))
-
+    walk.color(random_color())
 
 screen = Screen()
 screen.exitonclick()
