91d77792d1aef03d4c363516457420200b80d475
Negar Norouzi <negar.norouzi94@gmail.com>
2023-08-30 17:28:30 +0430

    hirst paiting

--- Day18/Hirst_Painting_Project/Hirst_painting_Project.py
+++ Day18/Hirst_Painting_Project/Hirst_painting_Project.py
@@ -1,19 +1,32 @@
+import random
 import turtle
 from turtle import Turtle, Screen
-import colorgram
 
-colors = colorgram.extract('images.jpeg', 20)
-rgb = []
+color_list = [(254, 254, 254), (28, 161, 195), (198, 159, 48), (176, 180, 184), (127, 198, 154), (163, 125, 184),
+              (243, 153, 191), (159, 24, 34), (109, 115, 120), (35, 177, 81), (5, 6, 8), (245, 238, 37), (223, 26, 119),
+              (237, 30, 40), (75, 64, 34), (241, 210, 223), (210, 236, 219), (196, 213, 218), (188, 160, 25),
+              (181, 0, 0)]
 
+turtle.colormode(255)
+painting = Turtle()
 
-for i in colors:
-    r = i.rgb.r
-    g = i.rgb.g
-    b = i.rgb.b
-    new_color = (r ,g ,b)
-    rgb.append(new_color)
+painting.speed("fast")
+painting.setheading(225)
+painting.forward(300)
+painting.setheading(0)
 
-print(rgb)
+dot_numbers = 100
+
+for i in range(1, dot_numbers):
+    painting.dot(20, random.choice(color_list))
+    painting.forward(50)
+
+    if i % 10 == 0:
+        painting.setheading(90)
+        painting.forward(50)
+        painting.setheading(180)
+        painting.forward(500)
+        painting.setheading(0)
 
 screen = Screen()
 screen.exitonclick()
