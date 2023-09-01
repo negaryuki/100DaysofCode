446d59e921dc32d0295746cbbac5a07cc69bb342
negaryuki <negar.norouzi94@gmail.com>
2023-09-01 16:09:14 +0330

    added movement to snake

--- Day20/main.py
+++ Day20/main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Turtle, Screen
 
 # setup Screen:
@@ -6,11 +7,12 @@ screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)  # Turns off the animation, however, in order to see anything, we need to use the update method later
 
 # Step 1: Create a snake:
 
 segment_distance = [(0, 0), (-20, 0), (-40, 0)]  # create a tuple to define snake segments location
-snake = []   # create an empty list to hold the segments later on
+snake = []  # create an empty list to hold the segments later on
 
 for position in segment_distance:
     segment = Turtle("square")
@@ -18,6 +20,19 @@ for position in segment_distance:
     segment.penup()
     segment.goto(position)
     snake.append(segment)
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg in range(len(snake) - 1, 0, -1):
+        new_x = snake[seg - 1].xcor()
+        new_y = snake[seg - 1].ycor()
+        snake[seg].goto(new_x, new_y)
+    snake[0].forward(10)
+
 
 
 screen.exitonclick()
