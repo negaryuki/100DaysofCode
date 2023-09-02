24f484ff7cca2dda145990b38b849af9ea24a50d
negaryuki <negar.norouzi94@gmail.com>
2023-09-02 15:36:57 +0330

    update scoreboard

--- Day20/main.py
+++ Day20/main.py
@@ -32,7 +32,7 @@ while game_on:
     # Detect snake collision
     if snake.head.distance(food) < 15:
         food.refresh()         # reposition food
-
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
--- Day20/scoreboard.py
+++ Day20/scoreboard.py
@@ -1,13 +1,22 @@
 from turtle import Turtle
 
+ALIGNMENTS = "center"
+FONT = ("Courier ", 22, "bold")
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.color("red")
+        self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center", font=("Arial ", 24, "bold"))
+        self.update_scoreboard()
         self.hideturtle()
 
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENTS, font= FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
