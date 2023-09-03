8f800978d44a9ee9b59b0c4bb69eba6e5306c8bb
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-03 15:05:57 +0330

    Update main.py

--- Day22/main.py
+++ Day22/main.py
@@ -1,7 +1,5 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
-
 # setup Screen:
 
 screen = Screen()
@@ -10,21 +8,25 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0). # controls animation, by setting 0 it removes the animation
 
-paddle = Paddle()
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
 game_on = True
 
 screen.listen()
-screen.onkey(paddle.up, "Up")
+
+# r_paddle movement control:
+	
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+
+#l_paddle movement control:
+	
+screen.onkey(l_paddle.up, "w")
+screen.onkey(_paddle.down, "s")
 
 while game_on:
 	screen.update() 
-
-
-
-screen.exitonclick()
-
-
-
 
 
 
