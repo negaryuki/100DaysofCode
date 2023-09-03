35aa57cb2aa8a5cd71bd020583723490975988d9
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-03 09:43:36 +0330

    Create main.py

---
+++ Day22/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+# setup Screen:
+
+screen = Screen()
+screen.setup(width=800,height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+
+
+
+
+
+
+screen.exitonclick()
