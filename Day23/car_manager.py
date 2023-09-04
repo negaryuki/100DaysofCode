ad5828a1deee118979371f2d4d9895d1b9fde6eb
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-04 11:59:25 +0330

    Update car_manager.py

--- Day23/car_manager.py
+++ Day23/car_manager.py
@@ -1,3 +1,5 @@
+from turtle import Turtle
+import random
 # Define Constants:
 	
 COLOR = ["red","orange","yellow","green","blue","purple"]
@@ -6,4 +8,25 @@ MOVE_INCREMENT = 10
 	
 	
 class CarManager:
-	pass
+	
+	def __init__(self):
+		self.all_cars= []
+
+	def create_car(self):
+		random_chance = random.randint(1,6)
+		
+		if random_chance ==1:         # every 6 times the while loop runs, a car will be generated. 
+		
+			new_car = Turtle("square")
+			new_car.shapesize(stretch_wid=1 , stretch_len=2). #20 x 400
+			new_car.penup()
+			new_car.color(random.choice(COLOR))
+			random_y = randint(-250,250)
+			new_car.goto(300,random_y)
+			self.all_cars.append(new_car)
+		
+	def move_cars(self):
+		
+		for car self.all_cars:
+			car.backward(STARTING_MOVE_DISTANCE)
+		
