9ed3350fde7759d0781ca7880d2856be65365edb
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-04 14:27:04 +0330

    Update player.py

--- Day23/player.py
+++ Day23/player.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
 # Define constants:
 STARTING_POSITION = 0
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 200
 
-class Player:
-	pass
+class Player(Turtle):
+	def __init__(self):
+		super().__init__()
+		self.shape("turtle")
+		self.penup()
+		self.go_to_start()
+		self.seheading(90) # Heading will head north 
+		
+		
+	def go_up(self):
+		self.forward(MOVE_DISTANCE)
+		
+	def go_to_start(self):
+		self.goto(STARTING_POSITION)
+				
+	def is_at_finishline(self):
+		if self.ycor() >280:
+			return True
+			
+		else:
+			return False
