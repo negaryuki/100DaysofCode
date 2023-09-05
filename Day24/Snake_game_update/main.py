bcc0fa2a15df9bbe4a39748e0d96848a43636225
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-05 16:02:41 +0330

    Create main.py

---
+++ Day24/Snake_game_update/main.py
@@ -0,0 +1,53 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+# setup Screen:
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)  # Turns off the animation, however, in order to see anything, we need to use the update method later
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard ()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+
+while game_on:
+    screen.update()  # update screen in order to show snake
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect snake collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()         # reposition food
+        snake.extend()  # add a new segment for each food
+        scoreboard.increase_score()
+
+    # Detect snake collision with wall
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+
+    # Detect snake collision with snake tail
+    # if head of snake collides with ANY segment
+    for segment in snake.snake[1::]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+
+
+
+
+
+screen.exitonclick()
