b1d3b6809a962463f84086003d2801f0a9e3e6e8
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-10 10:26:54 +0330

    Create lesson.py

---
+++ Day25/lesson.py
@@ -0,0 +1,13 @@
+# without panda library :
+#import csv
+
+#with open("weather_data.csv") as data_file:
+#  data = csv.reader(data_file)
+#  tempretures = []
+#  for row in data:
+#    if row[1] != "temp":
+#      tempretures.append(row[1])
+    
+   
+    
+  
