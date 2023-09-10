6920c5e10f16aab54a5e4689bef1bf516315bdf0
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-10 10:54:00 +0330

    Update lesson.py

--- Day25/lesson.py
+++ Day25/lesson.py
@@ -14,6 +14,34 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
+print(data) # is a "dataframe"
 
-print(data["temp"])
+print(data["temp"]) # is a"series"
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+
+average_temp = sum(temp_list) / len(temp_list)
+print(average_temp)
+
+
+# -------------------------------------------------------
+# alternative way:
+# In the Python pandas library, the .mean() function is used to calculate the mean (average) of numeric data in a DataFrame or Series.
+# You can apply it to a pandas DataFrame or Series to get the average value of the data.   
+   
+average = data["temp"].mean()  
+
+print(average)
+
+# -------------------------------------------------------
+
+max_value = data["temp"].max()
+
+print(max_value)
+
+# -------------------------------------------------------
