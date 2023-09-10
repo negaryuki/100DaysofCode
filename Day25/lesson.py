98918fc1916c129f0147b6d331ce5f5f2c337e17
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-10 10:30:03 +0330

    Update lesson.py

--- Day25/lesson.py
+++ Day25/lesson.py
@@ -7,7 +7,13 @@
 #  for row in data:
 #    if row[1] != "temp":
 #      tempretures.append(row[1])
-    
-   
-    
-  
+
+
+# with Pandas: less codes, better outcome
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
+
+print(data["temp"])
