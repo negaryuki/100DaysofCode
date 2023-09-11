b2d3f9366087c3d027596aaf9941e6d4ac256fee
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-11 11:14:13 +0330

    Update exercise.py

--- Day26/exercise.py
+++ Day26/exercise.py
@@ -5,3 +5,25 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 squared_numbers = [n**2 for n in numbers]
 
 print(squared_numbers)
+
+#--------------------------------------------------
+
+# EXERCISE 02 - write a list comprehension to vreate a new list called results. This new list should only contain the even numbers from the list numbers.
+
+results = [n for n in numbers if n % 2 == 0]
+
+print(results)
+
+#--------------------------------------------------
+
+# EXERCISE 03 - Take a look inside file1.txt and file2.txt They each contain a bunch of numbers, each number on a new line.  Create a list called "results2" which contains the numbers that are common in both files.
+
+with open("file1.txt") as file1:
+    file1_data = file1.readlines()
+
+with open("file2.txt") as file2:
+    file2_data = file2.readlines()
+
+results2 = [int(n.strip()) for n in file1_data if n in file2_data]
+
+print(results2)
