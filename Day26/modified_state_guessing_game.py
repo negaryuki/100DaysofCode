ea5e785c13db2c7e4bd544a08a227c82c8a2f451
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-11 11:21:04 +0330

    Create modified_state_guessing_game.py

---
+++ Day26/modified_state_guessing_game.py
@@ -0,0 +1,59 @@
+import turtle
+import pandas
+
+# setup screen:
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)  # Add shape to the screen in order to be available
+
+turtle.shape(image)  # change shape to img file
+
+# extract data from csv file:
+data = pandas.read_csv("50_states.csv")
+
+all_states = data.state.to_list()
+
+
+# -------------------------------------------------
+
+# This function helps to find the xcor and ycor in a picture whenever you click on it
+
+# def get_mouse_click_coor(x,y):
+#   print(x,y)
+# turtle.onscreenclick(get_mouse_click_coor())  
+
+# -------------------------------------------------
+
+
+guessed_states = []
+while len(guessed_states) < 50:  # 50 states
+
+    # Pop-up to ask question:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="what's another State?").title()
+
+    if answer_state == "Exit":  # States to Learn
+        
+        missing_states = [state for state in all_states if state not in guessed_states]    # changed this lline with List comprehension 
+
+
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+
+        break  # there is no need for the turtle.mainloop anymore
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        
+        # create turtle to write down the name in the map
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]  # returns the info of the state that we chose as answer_state
+        xcor_states = int(state_data.x)
+        ycor_states = int(state_data.y)
+        t.goto(xcor_states, ycor_states)  # state_data.x ,state_data.y
+        t.write(answer_state)  # state_data.state.item(). --> returns the first element
+
+# turtle.mainloop() # is an alternative for screen.exitonclick(). continues program even after code has finished running
