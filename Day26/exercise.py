8e4a07060f785c3028e1021828cc31479c4489c9
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-11 15:37:05 +0330

    Update exercise.py

--- Day26/exercise.py
+++ Day26/exercise.py
@@ -39,4 +39,21 @@ result3 ={word:len(word) for word in split_sentence}
 
 print(result3) 
 
+#--------------------------------------------------
 
+# EXERCISE 05 - Use Dictionary Comprehension to create a dictionary called weather_f that takes each temperature in degree Celcius and converts it to Farenheit.
+# temp_f = (temp_c * 9/5 ) + 32
+
+weather_c = {
+  "Monday" : 12,
+  "Tuesday" :14,
+  "Wednesday" : 15,
+  "Thursday" :14,
+  "Friday" : 21,
+  "Saturday" :22,
+  "Sunday" :24,
+}
+
+weather_f = {day:(temp_c * 9/5 ) + 32 for day,temp_c in weather_c.items()}
+
+print(weather_f)
