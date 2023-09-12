04e7333129631c55f4976c3d55982026746cac4d
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-12 10:52:20 +0330

    Update lesson.py

--- Day27/lesson.py
+++ Day27/lesson.py
@@ -7,7 +7,7 @@ window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 #---------------------------------------------------
-# Label:
+# Creating a Label:
 # step 1 - create Label
   
 my_lable = tkingter.Lable(text="I Am a Label", font=("Arial", 24, "Bold"))  # this is not enough to display the lable
@@ -16,6 +16,23 @@ my_lable = tkingter.Lable(text="I Am a Label", font=("Arial", 24, "Bold"))  # th
 
 my_lable.pack(expand=True)  #arguments: expand,before, anchor,side, ....
   
+#---------------------------------------------------
+# Unlilmited Positional Arguments:
+
+def exmp(*args):  # This Function can accept any number of Arguments because of (*args)  
+  print(args[2])
+  for n in args:
+    print(n)
+
+# Example :
+
+def add(*args):
+  num_list =[number for number in args]
+  total = sum(num_list)
+  print(total)
+    
+add(2,5,4)
+
 #---------------------------------------------------
 
 window.mainloop() # acts like a while loop and keeps the window on the screen. this must always be at the end of the program
