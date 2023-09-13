6df1f601d5c5a4d5aacccd312eddc9ecf71cc623
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-13 10:43:13 +0330

    Update main.py

--- Day28/main.py
+++ Day28/main.py
@@ -15,3 +15,17 @@ LONG_BREAK_MIN = 20
 #------------------ COUNTDOWN MECHANISM ------------------
 
 #------------------- UI SETUP ----------------------------
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100,pady=50, bg=YELLOW)
+
+# Canvas Widget 
+
+canvas = Canvas(width= 200, height= 224, bg=YELLOW, highlightthickness=0)   # highlightthickness removes the gap between the canvas and window
+tomato_img = PhotoImage(file= "tomato.png")     # calls the image
+canvas.create_image(100,112, image=tomato_img)
+canvas.create_text(100,130, text= "00:00", fill= "white", font =(FONT_NAME, 35, "bold")
+canvas.pack()
+
+
+window.mainloop()
