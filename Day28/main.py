cc3d3312473c37cb1f535933a2baa88f73383b6f
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-09-13 16:29:08 +0330

    Update main.py

--- Day28/main.py
+++ Day28/main.py
@@ -11,8 +11,14 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps=0
+my_timer= None
 #------------------- TIMER RESET ------------------------
 def reset_timer():
+  window.after_cancel(my_timer)
+  canvas.itemconfig(timer_text, text = "00:00")
+  title_label.config(text = "Timer")
+  check_marks.config(text="")
+  
   
 #------------------- TIMER MECHANISM ---------------------
 def start_timer():
@@ -25,15 +31,26 @@ def start_timer():
   
   if reps % 8 ==0:
     count_down(long_break_sec)
+    title_label.config(text="Break", fg= RED)
+    
   elif reps % 2 == 0:
     count_down(short_break_sec)
+    title_label.config(text = "Break" , fg= PINK)
+  
   else:
     count_down(work_sec)
+        title_label.config(text= "WORK" , fg= GREEN)
+    marks= ""
+    work_session = math.floor(reps/2)
+       
+        for i in range(work_session):
+          mark += "✔"
+          checkmark_label.config(text=marks, font=(FONT_NAME, 22, "bold"))
   
 #------------------ COUNTDOWN MECHANISM ------------------
 
 def count_down(count):
-  
+  global my_timer
   
   count_min = math.floor(count/60)    #Returns the largest integer <= X exp. 4.8 = 4
   count_sec = count % 60
@@ -45,7 +62,7 @@ def count_down(count):
   canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}" )    #changing the text of a canvas is different from a label (.config) we have to use ".itemconfig"
   
   if count > 0:
-    window.after(1000,count_down, count - 1 )  # a method that takes an amount of time that should wait and after that calls a function
+    my_timer=window.after(1000,count_down, count - 1 )  # a method that takes an amount of time that should wait and after that calls a function
 
 
 #------------------- UI SETUP ----------------------------
@@ -67,11 +84,11 @@ canvas.grid(column=1, row=1)
 
 #------------------- Label ----------------------------  
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 50, "bold"), bg=YELLOW, fg=GREEN)   # fg = foreground which defines the color of the text 
+title_label = Label(text="Timer", font=(FONT_NAME, 50, "bold"), bg=YELLOW, fg=GREEN)   # fg = foreground which defines the color of the text 
 
-timer_label.grid(column=0, row=1)   # Place the label on grid
+title_label.grid(column=0, row=1)   # Place the label on grid
 
-checkmark_label = Label(text="✔", font=(FONT_NAME, 22, "bold"), fg=GREEN, bg=YELLOW)
+checkmark_label = Label(fg=GREEN, bg=YELLOW)
 
 checkmark_label.grid(column=1, row=3)   # Place the label on grid
 
@@ -80,7 +97,7 @@ checkmark_label.grid(column=1, row=3)   # Place the label on grid
 start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0, row=2, command = start_timer)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command = reset_timer
 reset_button.grid(column=2, row=2)
 
 window.mainloop()
