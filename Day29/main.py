0f3cd16837d67ffadbe8d223479646621575db8c
negaryuki <negar.norouzi94@gmail.com>
2023-09-16 13:31:44 +0330

    Update

--- Day29/main.py
+++ Day29/main.py
@@ -7,10 +7,6 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Generator")
-window.config()
-
-window = Tk()
-window.title("Pomodoro")
 window.config(padx=20, pady=20)
 
 # ------------------- Canvas Widget ----------------------------
@@ -19,8 +15,18 @@ canvas = Canvas(width=200,height=200)
 pass_img = PhotoImage(file="logo.png")
 canvas.create_image(100,100,image=pass_img)
 
-canvas.pack()
-#anvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.grid(row=0,column=1)
+
+# ------------------- Label ------------------------------------
+
+website_label = Label(text="Website:")
+website_label.grid(row=1,column=0)
+
+mail_user_label = Label(text="Email/Username:")
+mail_user_label.grid(row=2,column=0)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3,column=0)
 
 
 window.mainloop()
