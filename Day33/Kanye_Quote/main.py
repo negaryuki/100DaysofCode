af5ee06fd63c191cfe3f18f0dbbc9803a689d717
negaryuki <negar.norouzi94@gmail.com>
2023-09-24 23:23:12 +0330

    Update

--- Day33/Kanye_Quote/main.py
+++ Day33/Kanye_Quote/main.py
@@ -7,7 +7,6 @@ def get_quote():
     response.raise_for_status()
     data = response.json()
     quote = data["quote"]
-
     canvas.itemconfig(quote_text, text=quote)
 
 
@@ -16,13 +15,13 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file= "background.png")
+background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(150, 207, text="Kanye quote", width=250, font="Arial")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button = Button(image=kanye_img, highlightthickness=1, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
 window.mainloop()
