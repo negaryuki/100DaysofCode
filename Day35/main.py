64d68bddcc2e8e3181a67fb7b431dfd53f839be9
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-10-01 15:28:41 +0330

    Update main.py

--- Day35/main.py
+++ Day35/main.py
@@ -2,14 +2,31 @@ import requests
 
 
 OWM_endpoint  = "https://api.openweathermap.org/data/3.0/onecall"
-api_key = "c80067c09286776bc601fa3c8cdf80c5"
+api_key = "024ac76d4f00bf1ef45f21ae9d7b3c84"
 
 weather_params = {
   "lat": 51.507351,
   "lon": -0.127758,
   "appid": api_key,
+  "exclude": "current,minutely,daily"
 }
 
 response = requests.get(OWM_endpoint, params= weather_params)
+response.raise_for_status()
+weather_data = response.json()
 
-print(response.status_code)
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+  condition_code = (hour_data["weather"][0]["id"])
+  if int(condition_code) < 700:
+    will_rain = True
+
+if will_rain:
+  print("bring an umbrella") 
+
+
+
+
