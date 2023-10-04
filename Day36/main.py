e428c647d9ddb537e99e9f38533044ed89b3f3a3
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-10-04 11:30:57 +0330

    Update main.py

--- Day36/main.py
+++ Day36/main.py
@@ -2,10 +2,12 @@ import requests
 
 
 STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 ALPHA_VANTAGE_APIKEY = "****"
 NEWS_APIKEY = "****"
+
 
 #----------------- PARAMETERS -----------------------
 
@@ -16,16 +18,15 @@ alpha_vantage_parameters = {
 }
 
 news_api_parameters = {
-    "q": f"{STOCK_NAME} Market Update",
-    "pageSize": 3,
     "apiKey": NEWS_APIKEY,
+    "qInTitle": COMPANY_NAME,
 }
 
 #----------------- REQUESTS  -----------------------
-
 alpha_vantage_response = requests.get(STOCK_ENDPOINT, params=alpha_vantage_parameters)
 alpha_vantage_response.raise_for_status()
 stock_data = alpha_vantage_response.json()
+
 
 news_api_response = requests.get(NEWS_ENDPOINT, params=news_api_parameters)
 news_api_response.raise_for_status()
@@ -34,6 +35,7 @@ news_data = news_api_response.json()
 #-------------------------------------------------
 
 data_list = [value for (key,value) in stock_data.items()]
+
 
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4.close"]
@@ -46,6 +48,13 @@ day_before_yesterday_closing_price = day_before_yesterday_data["4.close"]
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price)0
 
 difference_percent = (difference / float(yesterday_closing_price)) * 100
+
+
+#-------------------------------------------------
+
+articles = news_data["articles"]
+three_articles = articles[:3]
+
 
 if difference_percent > 5:
   print("Get News")
