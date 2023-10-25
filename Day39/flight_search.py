d5e18bdb7c0dce9eb1090a05074f5a03bab54c7c
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-10-25 15:15:31 +0330

    Update flight_search.py

--- Day39/flight_search.py
+++ Day39/flight_search.py
@@ -1,23 +1,51 @@
 import requests
+from flight_data import FlightData
+
 #-----------API INFO----------
 
-TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY= "TEQUILA_API_KEY"
+TEQUILA_ENDPOINT = "https://tequilaA-api.kiwi.com"
+TEQUILA_API_KEY= "API"
+
+
 
 class FlightSearch:
-  
-  def get_destination_code(self,city_name):
-    
-    location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
-    headers = {"apikey": TEQUILA_API_KEY}
-    query = {
-      "term": city_name,
-      "location_type": "city"
-    }
-    
-    response = requests.get(url= location_endpoint, headers = headers, params = query)
-    
-    result = response.json()["location"]
-    code = results[0]['code']
-    
-    return code
+
+    def get_destination_code(self, city_name):
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
+        
+        
+    def check_flights(self, origin_city_code,       destination_city_code, from_time, to_time):
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
+        
+      def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+          headers = {"apikey": TEQUILA_API_KEY}
+          query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
