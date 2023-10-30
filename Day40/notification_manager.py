301d0741805069f993849e7f3908b88953ba3220
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-10-30 09:52:45 +0330

    Create notification_manager.py

---
+++ Day40/notification_manager.py
@@ -0,0 +1,10 @@
+  def send_emails(self, emails, message):
+        with smtplib.SMTP(EMAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}".encode('utf-8')
+                )
