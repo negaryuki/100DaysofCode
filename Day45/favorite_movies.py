65bfd34be3f77188109d18278237cb23aef35968
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-11-05 16:42:41 +0330

    Create favorite_movies.py

---
+++ Day45/favorite_movies.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movies.getText() for movies in all_movies]
+
+#reverse list:
+reversed_list = movie_titles[::-1]
+
+ 
+print(movie_titles)
+
+print(all_movies)
+
+
+with open("movies.text", "w") as file:
+  for movie in reversed_list:
+    file.write(f"{movie}/n")
