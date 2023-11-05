b137cb5c2b93b3fdd225d6a4f34261c658d5f7a1
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-11-05 13:32:49 +0330

    Create lesson02.py

---
+++ Day45/lesson02.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+response = requests.get("https://news.ycombinator.com/news")
+
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+
+articles = soup.find_all(name= "a", class_="storyline")
+
+article_texts = []
+article_links =[]
+
+for article_tag in articles:  
+  article_text = article_tag.getText()
+  article_texts.append(article_text)
+  
+  article_link= article_tag.get("href")
+  article_links.append(article_link)
+  
+  
+article_upvotes = [int(score.getText().split[0]) for score in soup.find_all(name="span", class_= "score")]
+
+largest_number =max(article_upvotes)
+largest_index= article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_texts)
+print(article_links)
+print(article_upvotes)
+
+
+
