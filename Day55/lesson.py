00446c1c8b6bcb919fca3f1c2bd7b33bc5ce9533
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-11-13 09:37:53 +0330

    Update lesson.py
    Added Decorators

--- Day55/lesson.py
+++ Day55/lesson.py
@@ -2,23 +2,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+      
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+      
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
 
 @app.route("/")
 def hello_world():
-    return '<h1 style = "text-align:center">Hello, World!</h1>' \
-    '<p>This is a Paragraph</p>' \
-    '<img src="https://media.giphy.com/media/c76IJLufpNwSULPk77/giphy.gif" width=200>'
+    return '<h1 style="text-align: center">Hello, World!</h1>' \
+           '<p>This is a Paragraph</p>' \
+           '<img src="https://media.giphy.com/media/c76IJLufpNwSULPk77/giphy.gif" width=200>'
 
- 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def bye():
     return "Bye!"
 
-
 @app.route("/<name>/<int:number>")
 def greet(name, number):
-    return f"Hello there {name} ! you are {number} years old"
-
+    return f"Hello there {name}! You are {number} years old"
 
 if __name__ == "__main__":
     app.run(debug=True)
