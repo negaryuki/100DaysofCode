436a65d957dfd9c8b63844af30ae7abe661ff9a5
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2023-11-13 10:02:55 +0330

    Update lesson.py
    Added advanced decorators

--- Day55/lesson.py
+++ Day55/lesson.py
@@ -36,3 +36,25 @@ def greet(name, number):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+## Advanced Decorators:
+  
+class User:
+  def __init__(self,name):
+    self.name = name
+    self.is_logged_in = False
+    
+def is_authenticated_decorator(function):
+  def wrapper(*args,**kwargs):
+    if args[0].is_logged_in == True:
+      function(arg[0])
+  return wrapper()
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+  print(f"This is {user.name}'s new blog post")
+  
+new_user = User("Negar")
+new_user.is_logged_in = True
+create_blog_post(new_user)
