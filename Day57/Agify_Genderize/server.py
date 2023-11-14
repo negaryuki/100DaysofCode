880ab6bfbf3d826d7bb542b08330933b61436be2
negaryuki <negar.norouzi94@gmail.com>
2023-11-14 19:03:46 +0330

    debug

--- Day57/Agify_Genderize/server.py
+++ Day57/Agify_Genderize/server.py
@@ -1,22 +1,23 @@
-import flask
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
-    
+
+
 @app.route('/guess/<name>')
 def guess(name):
-  gender_url =f"http://api.genderize.io?name={name}"
-  gender_response = requests.get(gender_url)
-  gender_data= gender_response.json()
-  gender = gender_data["gender"]
-  
-  age_url= f"https://api.agify.io?{name}"
-  age_reponse = requests.get(age_url)
-  age_data = age_reponse.json()
-  age = age_data["age"] 
-   
-  return render_template("index.html", name =name, gender=gender, age=age)
-    
-    
-if __name__ =="__main__":
-  app.run(debug=True)
+    gender_url = f"http://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+
+    return render_template("index.html", name=name, gender=gender, age = age )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Day57/Lesson/server.py
+++ Day57/Lesson/server.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template
 import random
-import time
+import datetime
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    random_number = random.randint(0,10)
+    random_number = random.randint(0, 10)
     current_year = datetime.datetime.now().year
     return render_template("index.html", num=random_number, year=current_year)
-    
-if __name__ =="__main__":
-  app.run(debug=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
   
   
 @app.route('/blog')
