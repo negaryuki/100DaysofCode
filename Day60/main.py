b6fc26831ece36d5f30dcb9c3fd3c8ac35116b8c
negaryuki <negar.norouzi94@gmail.com>
2023-11-23 07:46:28 +0330

    update

--- Day60/main.py
+++ Day60/main.py
@@ -8,5 +8,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
