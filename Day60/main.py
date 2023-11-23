3fe556164b93f6a497bc631beb116291f3705e51
negaryuki <negar.norouzi94@gmail.com>
2023-11-23 07:48:35 +0330

    update

--- Day60/main.py
+++ Day60/main.py
@@ -8,9 +8,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
+@app.route("/login", methods=["POST"])
+def receive_data():
+    return "Success! Form submitted!!"
+
 
 
 if __name__ == "__main__":
