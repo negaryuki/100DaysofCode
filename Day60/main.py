32cd59384797f8c83bc8dde6846f6cee5ba6f7bc
negaryuki <negar.norouzi94@gmail.com>
2023-11-23 07:57:24 +0330

    update

--- Day60/main.py
+++ Day60/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,8 +10,9 @@ def home():
 
 @app.route("/login", methods=["POST"])
 def receive_data():
-    return "Success! Form submitted!!"
-
+    name = request.form["username"]
+    password = request.form["password"]
+    return f'<h1>Name:{name}, Password:{password}</h1>'
 
 
 if __name__ == "__main__":
