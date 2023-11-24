3230791a9ecc16e791398dcb248b69536cc9bb9e
negaryuki <negar.norouzi94@gmail.com>
2023-11-24 07:42:37 +0330

    update

--- Day60/main.py
+++ Day60/main.py
@@ -1,18 +1,43 @@
 from flask import Flask, render_template, request
+import requests
+
+posts = requests.get("https://api.npoint.io/65afe472604da20eef6d").json()
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route('/form-entry', methods=["POST"])
 def receive_data():
-    name = request.form["username"]
-    password = request.form["password"]
-    return f'<h1>Name:{name}, Password:{password}</h1>'
+    data = request.form
+    print(data["name"])
+    print(data["email"])
+    print(data["phone"])
+    print(data["message"])
+    return "<h1>Successfully sent your message</h1>"
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
