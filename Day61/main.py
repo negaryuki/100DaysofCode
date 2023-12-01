8a07f66543a67adc20d6bec2452b9c88910f1a54
negaryuki <negar.norouzi94@gmail.com>
2023-12-01 13:55:58 +0330

    update

--- Day61/main.py
+++ Day61/main.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
 
-On Windows type:
-python -m pip install -r requirements.txt
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = StringField('Password')
 
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
+app.secret_key = "secret"
 
 
 @app.route("/")
@@ -23,7 +20,8 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
