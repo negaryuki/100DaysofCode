ccc2b883728cff5369e08d6f80e0fe5af4a0d22d
negaryuki <negar.norouzi94@gmail.com>
2023-12-03 21:47:29 +0330

    update

--- Day61/main.py
+++ Day61/main.py
@@ -22,8 +22,12 @@ def home():
 @app.route("/login", methods=["GET","POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
-    return render_template('login.html', form=login_form)
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == '__main__':
