310c2126d98b03e0d9b432c3ea30d6f718a006f7
negaryuki <negar.norouzi94@gmail.com>
2023-12-08 14:27:04 +0330

    update

--- Day63/main.py
+++ Day63/main.py
@@ -7,19 +7,18 @@ all_books = []
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = [
-            {
+        new_book = {
+
                 "title": request.form["title"],
                 "author": request.form["author"],
                 "rating": request.form["rating"],
             }
-        ]
         all_books.append(new_book)
         return redirect(url_for('home'))
 
