83fcc62e6966d742423e1441a2c83b00cb748571
negaryuki <negar.norouzi94@gmail.com>
2023-12-09 20:41:27 +0330

    update

--- Day63/main.py
+++ Day63/main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.d"
+db = SQLAlchemy()
 db.init_app(app)
 
 all_books = []
