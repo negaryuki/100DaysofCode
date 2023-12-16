a399daf7bf9ae482f005fed081afe84a98f26061
negaryuki <negar.norouzi94@gmail.com>
2023-12-16 10:48:49 +0330

    update

--- Day66/main.py
+++ Day66/main.py
@@ -25,24 +25,17 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+def convert_to_dict(self):
+    dictionary = {}
+    return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
 @app.route("/random")
 def get_random_cafe():
     result = db.session.execute(db.select(Cafe))
     all_cafes = result.scalars().all()
     random_cafe = random.choice(all_cafes)
-    return jsonify(cafe={
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price
-    })
+    return jsonify()
 
 
 with app.create_contect():
