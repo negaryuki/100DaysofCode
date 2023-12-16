230f6486134a2dd08797c7ccc280f0cc77e0872d
negaryuki <negar.norouzi94@gmail.com>
2023-12-16 10:31:38 +0330

    update

--- Day64/main.py
+++ Day64/main.py
@@ -1,115 +1,32 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-##CREATE DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
 
+On Windows type:
+python -m pip install -r requirements.txt
 
-##CREATE TABLE
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    rating = db.Column(db.Float, nullable=True)
-    ranking = db.Column(db.Integer, nullable=True)
-    review = db.Column(db.String(250), nullable=True)
-    img_url = db.Column(db.String(250), nullable=False)
-db.create_all()
+On MacOS type:
+pip3 install -r requirements.txt
 
- new_movie = Movie(
-     title="Phone Booth",
-     year=2002,
-     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-     rating=7.3,
-     ranking=10,
-     review="My favourite character was the caller.",
-     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
- )
-db.session.add(new_movie)
-db.session.commit()
+This will install the packages from requirements.txt for this project.
+'''
 
-class RateMovieForm(FlaskForm):
-    rating = StringField("Your Raiting Out of 10")
-    review = StringField("Your Review")
-    submit = SubmitField("Done")
-
-class FindMovieForm(FlaskForm):
-    title = StringField("Movie Title", validators=[DataRequired()])
-    submit = SubmitField("Add Movie")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
 
 
 @app.route("/")
 def home():
-    all_movies = Movie.query.order_by(Movie.rating).all()
-
-    for i in range(len(all_movies)):
-        all_movies[i].ranking = len(all_movies) - i
-        db.session.commit()
-    return render_template("index.html", movies=all_movies)
-
-
-@app.route('/edit')
-def rate_movie():
-    form = RateMovieForm()
-    movie_id = request.args.get(movie_id)
-    movie= Movie.query.get(movie_id)
-    if form.validate_on_submit():
-        movie.rating = float(form.rating.data)
-        movie.review = form.review.data
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template ("edit.html", movie =movie, form=form)
-
-
-@app.route('/delete')
-def delete_movie():
-    movie_id=requests.args.get("id")
-    movie=Movie.query.get(movie_id)
-    db.session.delete(movie)
-    db.session.commit()
-    return redirect (url_for('home'))
-
-@app.route('/add', methods=["GET","POST"])
-def add_movie():
-    form = FindMovieForm()
-
-    if form.validate_on_submite():
-        movie_title = form.title
-        response= requests.get(MOVIE_DB_SEARCH_URL, params={
-            "api_key": MOVIE_DB_API_KEY,
-            "query": movie_title})
-        data = response.json()["results"]
-        return render_template("select.html", options=data)
-    
-    return render_template("add.html", form = form)
-
-@app.route('/find')
-def find_movie():
-    movie_ip_id = requests.args.get("id")
-    if movie_api_id:
-        movie_api_url = f'{MOVIE_DB_INFO_URl}/{movie_api_id}'
-        response =. requests.get(movie_api_url, params={
-        "api_key": MOVIE_DB_API_KEY
-        }
-        data = response.json()
-            new_movie= Movie(
-            title=data["title"],
-            year=data["release_date"].split("-")[0],
-            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
-            descrption=data["overview"]
-        )
-        db.session.add(new_movie)
-        db.session.commit()
-        return redirect(url_for("home"))
-                            
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
---
+++ Day66/main.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+
+@app.route("/random")
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return render_template("random.html", cafe=cafe)
+
+
+with app.create_contect():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+## HTTP GET - Read Record
+
+## HTTP POST - Create Record
+
+## HTTP PUT/PATCH - Update Record
+
+## HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
