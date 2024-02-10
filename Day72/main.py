3e1d553223c2c62fafac67d107048f8dbd18b133
negaryuki <negar.norouzi94@gmail.com>
2024-02-10 19:17:03 +0330

    update

--- Day72/main.py
+++ Day72/main.py
@@ -16,3 +16,11 @@ print(clean_df['Starting Median Salary'].max())
 print(clean_df['Starting Median Salary'].idxmax())
 print(clean_df['Undergraduate Major'].loc[43])
 print(clean_df.loc[43])
+
+
+# ------------The Highest Mid-Career Salary---------------
+
+print("What college major has the highest mid-career salary? How much do graduates with this major earn?")
+print(f"Highest Mid-career salary:{clean_df['Mid-Career Median Salary'].max()}")
+print(f"Index for the max mid career salary: {clean_df['Mid-Career Median Salary'].idxmax()}")
+print(f"college major with highest mid-career salary: {clean_df['Undergraduate Major'][8]}")
