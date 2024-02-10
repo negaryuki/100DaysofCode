027fa3e80c1cf10353db0355ece4af9c5d044a0c
negaryuki <negar.norouzi94@gmail.com>
2024-02-10 19:01:12 +0330

    update

--- Day72/main.py
+++ Day72/main.py
@@ -1,2 +1,18 @@
 import pandas as pd
-df = pd.read_csv('salaries_by_college_major.csv')
+df = pd.read_csv('salaries-by-college-major.csv')
+
+
+#print(df.head())
+#print(df.shape)
+#print(df.columns)
+
+#print(df.isna())
+#print(df.tail())
+
+clean_df = df.dropna()
+#print(clean_df.tail())
+
+print(clean_df['Starting Median Salary'].max())
+print(clean_df['Starting Median Salary'].idxmax())
+print(clean_df['Undergraduate Major'].loc[43])
+print(clean_df.loc[43])
