1812854d2d56089001d83f5b0d32f946a649ccae
Negar Yuki <49559503+negaryuki@users.noreply.github.com>
2024-02-12 11:08:35 +0330

    Update main.py

--- Day74/main.py
+++ Day74/main.py
@@ -31,3 +31,29 @@ most_parts =sets.sort_values('num_parts', ascending=False).head()
 print(most_parts)
 
 
+themes_by_year = sets.groupby('year').agg({'theme_id': pd.Series.unique})
+themes_by_year.rename(columns={'theme_id':'nr_themes'}, inplace = True)
+
+print(themes_by_year.head())
+print(themes_by_year.tail())
+
+print(mark)
+
+plt.plot(themes_by_years.index[:-2], themes_by_year.nr_themes[:-2])
+
+ax1 = plt.gca() # getting the axis 
+ax2 = ax1.twinx()  #create another axis that shares the same x-axis
+
+
+ax1.plot(sets_by_year.index[:-2], sets_by_year.nr_themes[:-2], color='g')
+ax2.plot(themes_by_year.index[:-2], themes_by_year.nr_themes[:-2], color='b')
+
+
+ax1.set_xlabel('Year')
+ax1.set_ylabel('Number of Sets', color='green')
+ax2.set_ylabel('Number of Themes', color = 'blue')
+
+
+
+             
+  
