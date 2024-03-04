8d6a0e87091fc42375d6a2a6b24acff9b3d9c904
negaryuki <negar.norouzi94@gmail.com>
2024-03-04 23:03:16 +0330

    update

--- Day86 - Portfolio - GUI/main.py
+++ Day86 - Portfolio - GUI/main.py
@@ -2,13 +2,14 @@ import tkinter as tk
 import random
 import time
 
+
 class TypingTestTimeApp:
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = root
         self.root.title("Typing Speed Test")
         self.root.geometry("400x300")
 
-        self.sample_text =[
+        self.sample_text = [
             "The quick brown fox jumps over the lazy dog.",
             "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -32,7 +33,6 @@ class TypingTestTimeApp:
         self.text_input = tk.Text(self.root, height=5, width=50)
         self.text_input.pack()
 
-
         # Button
         self.start_btn = tk.Button(self.root, text="Start", command=self.start_typing_test)
         self.start_btn.pack()
@@ -44,11 +44,11 @@ class TypingTestTimeApp:
         # Restart Button
         self.restart_btn = tk.Button(self.root, text="Restart", command=self.restart_typing_test)
         self.restart_btn.pack()
-        self.restart_btn.pack_forget()   # Hide Restart Button
+        self.restart_btn.pack_forget()  # Hide Restart Button
 
     def start_typing_test(self):
         self.start_btn.pack_forget()
-        self.time_start = time.time()    # Record current time
+        self.time_start = time.time()  # Record current time
 
         # This line binds the <KeyRelease> event to the check_typing_speed method.
         # This means that every time a key is released (i.e., a key is typed) in the txt_input text widget,
@@ -59,9 +59,22 @@ class TypingTestTimeApp:
         if self.text_input.get("1.0", "end-1c") == self.current_text:
             self.time_end = time.time()
             time_taken = self.time_end - self.time_start
-            words_per_minute = int((self.total_words/time_taken)* 60)
+            words_per_minute = int((self.total_words / time_taken) * 60)
             self.result_label.config(text=f"Your Typing speed is {words_per_minute} words per minute.")
             self.text_input.unbind("<KeyRelease>")
             self.restart_btn.pack()
 
+    def restart_typing_test(self):
+        self.current_text = random.choice(self.sample_text)
+        self.total_words = len(self.current_text)
+        self.test_text_label.config(text=self.current_text)
+        self.text_input.delete("1.0", "end")
+        self.result_label.config(text="")
+        self.restart_btn.pack_forget()
+        self.start_btn.pack()
 
+
+if __name__ == "main":
+    root = tk.Tk()
+    app = TypingTestTimeApp(root)
+    root.mainloop()
