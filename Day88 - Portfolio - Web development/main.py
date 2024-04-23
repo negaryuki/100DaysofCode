3691268942e2fec5d6ba1c92bf44e8ae84b7ebe1
negaryuki <negar.norouzi94@gmail.com>
2024-04-23 19:55:43 +0330

    starting project

--- Day88 - Portfolio - Web development/main.py
+++ Day88 - Portfolio - Web development/main.py
@@ -5,25 +5,25 @@ app = Flask(__name__)
 
 # ------- Connect to DB -------------
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
-db = SQLAlchemy()
-db.init_app(app)
+db = SQLAlchemy(app)  # Initialize db within the app context
 
 
 # ----- Cafe TABLE Configuration -----
-
 class Cafe(db.Model):
-    class Cafe(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(250), unique=True, nullable=False)
-        map_url = db.Column(db.String(500), nullable=False)
-        img_url = db.Column(db.String(500), nullable=False)
-        location = db.Column(db.String(250), nullable=False)
-        has_sockets = db.Column(db.Boolean, nullable=False)
-        has_toilet = db.Column(db.Boolean, nullable=False)
-        has_wifi = db.Column(db.Boolean, nullable=False)
-        can_take_calls = db.Column(db.Boolean, nullable=False)
-        seats = db.Column(db.String(250), nullable=False)
-        coffee_price = db.Column(db.String(250), nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def convert_to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
 @app.route('/')
@@ -33,8 +33,7 @@ def home():
 
 @app.route('/all')
 def get_all_cafes():
-    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
-    all_cafes = result.scalars().all()
+    all_cafes = Cafe.query.order_by(Cafe.name).all()
     return jsonify(cafes=[cafe.convert_to_dict() for cafe in all_cafes])
 
 
