a654ebac47e71b69132a62a70423c3220bb17318
negaryuki <negar.norouzi94@gmail.com>
2024-05-12 22:42:38 +0330

    update

--- Day89 - Portfolio - To Do List/app.py
+++ Day89 - Portfolio - To Do List/app.py
@@ -1,30 +1,34 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 # ------- Connect to Tasks DB -------------
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-db = SQLAlchemy(app)  # Initialize db within the app context
 app.config['SECRET_KEY'] = "secretsecret"
+db = SQLAlchemy(app)
 Bootstrap(app)
 
-#----------------------------
 
+# Define the Tasks model
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(200))
-    completed = db.Column(db.Boolean,default=False)
+    completed = db.Column(db.Boolean, default=False)
+
+
+# Create database tables
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def home():
     tasks = Tasks.query.all()
-    return render_template('index.html', tasks= tasks)
-
+    return render_template('index.html', tasks=tasks)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
